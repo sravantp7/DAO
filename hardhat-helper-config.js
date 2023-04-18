f1793a9bb20bea1af6b6c7f0fcd20d0913122023
Sravan T P <sravantp03@gmail.com>
2023-04-18 15:47:57 +0530

    created Governor Contract and added helper config

---
+++ hardhat-helper-config.js
@@ -0,0 +1,9 @@
+const VOTING_DELAY = 1; // 1 block
+const VOTING_PERIOD = 50400; // 1 week in blocks
+const QUORUM_PERCENTAGE = 4;
+
+module.exports = {
+  VOTING_DELAY,
+  VOTING_PERIOD,
+  QUORUM_PERCENTAGE,
+};
